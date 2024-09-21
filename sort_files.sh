991a475fb3bd4ccf9495eb94304fefe35c123251
jahusv <125425367+jahusv@users.noreply.github.com>
2024-09-21 14:12:04 +0700

    Update sort_files.sh

--- sort_files.sh
+++ sort_files.sh
@@ -2,13 +2,11 @@
 
 if [ -z "$1" ]; then
     echo "Директория не указана"
-    exit 1
 fi
 
 if [ ! -d "$1" ]; then
     echo 'Директория не существует' 
-    exit 1
 fi 
 
 echo "Список файлов:"
-ls -lt "$1" | grep '^-'
+ls -lt "$1" 
